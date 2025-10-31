f26cb13dc4fbc28eb779031558d4d179c2aa6b3c
aftab-alam02526 <aftab0447@gmail.com>
2025-10-31 11:53:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import clickCounter from "./components/index";
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+      <clickCounter />
     </div>
   )
 }
